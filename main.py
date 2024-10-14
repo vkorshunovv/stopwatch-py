809799ca3a1962ca2fde6c4a126d2f4dfd4415dd
Viktor Korshunov <viktorkorshunov@MacBook-Air-Viktor.local>
2024-10-14 14:08:14 +0200

    define class constructor

--- main.py
+++ main.py
@@ -5,6 +5,16 @@ from PyQt5.QtCore import QTimer, QTime, Qt
 class Stopwatch(QWidget):
     def __init__(self):
         super().__init__()
+        self.time = QTime(0,0,0,0)
+        self.time_label = QLabel("00:00:00:00")
+        self.start_button = QPushButton("Start", self)
+        self.stop_button = QPushButton("Stop", self)
+        self.reset_button = QPushButton("Reset", self)
+        self.timer = QTimer(self)
+        self.initUI()
+        
+    def initUI():
+        pass 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
